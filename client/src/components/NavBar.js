2744f5ed78e361d11e21746c60f4e3f3a5411ce8
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-01 19:29:59 -0700

    Updated NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,7 @@ import { Flex, UnorderedList, ListItem, Link } from "@chakra-ui/react";
 const NavBar = () => {
 
     return (
-        <Flex bg={'primary.50'} w={"full"} h={10} justifyContent='flex-end'>
+        <Flex bg={'primary.100'} w={"full"} h={10} justifyContent='flex-end'>
             <UnorderedList styleType={"none"}>
                 <ListItem p={2}>
                     <Link style={{ textDecoration: 'none' }} mx={2} href="/home">
