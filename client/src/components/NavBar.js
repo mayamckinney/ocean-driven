597f2b50f1ec84823d3083304aa470846535e494
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-03-02 15:53:36 -0700

    Updated styling for NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,60 @@
 import React from "react";
-import { Flex, UnorderedList, ListItem, Link } from "@chakra-ui/react";
+import { Box, Flex, VStack, HStack, UnorderedList, ListItem, Link, Image } from "@chakra-ui/react";
 
 
 const NavBar = () => {
 
     return (
-        <Flex bg={'primary.100'} w={"full"} h={10} justifyContent='flex-end'>
-            <UnorderedList styleType={"none"}>
-                <ListItem p={2}>
-                    <Link style={{ textDecoration: 'none' }} mx={2} href="/home">
-                        Home
-                    </Link>
-                    <Link style={{ textDecoration: 'none' }} mx={2} href="/profile">
-                        Profile
-                    </Link>
-                    <Link style={{ textDecoration: 'none' }} mx={2} href="/login">
-                        Login
-                    </Link>
-                    <Link style={{ textDecoration: 'none' }} mx={2} href="/signup">
-                        Sign Up
-                    </Link>
-                    <Link style={{ textDecoration: 'none' }} mx={2} href="/logout">
-                        Logout 
-                    </Link>
-                </ListItem>
-            </UnorderedList>
+        <Flex
+            bg='white'
+            w={"full"}
+            justifyContent='space-between'
+            pos='fixed'
+            zIndex={1}
+            boxShadow='base'
+            p={2}
+        >
+            <Box alignSelf='center'>
+                <Image
+                    src="/images/DALL·E_icon_placeholder1.png"
+                    alt="yacht icon"
+                    w={12}
+                    borderRadius={24}
+                />
+            </Box>
+
+            <Flex flexDirection='column' justifyContent='flex-end'>
+        
+                <UnorderedList styleType='none' display='flex' mb={2}>
+                    <ListItem>
+                        <Link style={{ textDecoration: 'none' }} mr={3} _hover={{ fontWeight: 'semibold', color: 'secondary.700' }} href="/login" >
+                            Login/Signup
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                    </ListItem>
+                    <ListItem>
+                        <Link style={{ textDecoration: 'none' }} mr={4} _hover={{ fontWeight: 'semibold', color: 'secondary.700' }}  href="/logout">
+                            Logout
+                        </Link>
+                    </ListItem>
+                </UnorderedList>
+
+                
+                <UnorderedList styleType={"none"} display='flex' justifyContent='flex-end'>
+                    <ListItem>
+                        <Link style={{ textDecoration: 'none' }} fontSize='xl' mr={2} py={1} px={2} borderRadius='md' _hover={{ background: 'secondary.100', fontWeight: 'semibold' }} href="/home">
+                            Home
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Link style={{ textDecoration: 'none' }} fontSize='xl' mr={2} py={1} px={2} borderRadius='md' _hover={{ background: 'secondary.100', fontWeight: 'semibold' }} href="/profile">
+                            Profile
+                        </Link>
+                    </ListItem>
+                </UnorderedList>
+            </Flex>
+
         </Flex>
     )
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
 // Test page to see if Chakra UI is working
 import BoatsTest from "./BoatsTest";
 
+// import img from '../../public/images/'
+
 const Home = () => {
   return (
     <Box>
+      <Box>
+        <Image 
+          src="/images/placeholder-coast.jpeg" 
+          alt="a beautiful coast" 
+          w={'full'}
+          h={{ base: '50vh', lg:'100vh'}}
+          objectFit='cover'
+          />
+      </Box>
+      
       <BoatsTest />
     </Box>
   );
